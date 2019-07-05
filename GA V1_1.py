66988ae511c2b8787e4ac5e724224979ba8fde55
Ronan <ronancraig12@gmail.com>
2019-07-05 14:01:30 +0100

    setting up functions
    haven't got variables to pass into fitness() yet

--- GA V1_1.py
+++ GA V1_1.py
@@ -1,4 +1,5 @@
 """ Attempting to create the GA PID controller for the MSD system """
+#Created initial population
 
 import numpy as np
 from scipy import signal
@@ -8,7 +9,7 @@ from pylab import plot,xlabel,ylabel,title,legend,figure,subplots
 import matplotlib.pyplot as plt
 import random
 
-def initialise(cltf, ):
+def initialise():
     #first thing is to create the system itself - MSD, input
     #MSD state definitions
     m = 2 # kg   - mass
@@ -23,51 +24,12 @@ def initialise(cltf, ):
     # Transfer function definitions
     MSDnum = [0, 0, 1]
     MSDden = [m, c, k]
-    GAnum = [kd, kp, ki]
-    GAden = [0, 1, 0]
-    GHs_num = signal.convolve(GAnum, MSDnum)
-    GHs_den = signal.convolve(GAden, MSDden)
-    print("GHs_num: ", GHs_num)
-    print("GHs_den: ", GHs_den)
 
-    cltf_num = GHs_num
-    cltf_den = GHs_den + GHs_num
-    cltf = signal.TransferFunction(cltf_num, cltf_den)
-    #Error signal is input - output = input(1 - GA*MSD)
-    """error_num = GHs_den - GHs_num
-    error_den = GHs_den
-    errorsig = signal.TransferFunction(error_num, error_den)
-    print("Error_num: ", error_num)
-    print("error_den: ", error_den)
-    print("errorsig: ", errorsig) """
-    
-    #Creating timestep table for step functions
-    times=[]
-    stepplot=[]
-    f=0.000
-    n = 0.001
-    timesteps = 3000
-    for s in range(timesteps):
-        stepplot.append(1)
-        times.append(f)
-        f = f+n
 
-    unityF = signal.TransferFunction([1],[1])
-    # Step function in use
-    t1, y1 = signal.step(unityF, T=times) #signal.step(errorsig)
-    t2, y2 = signal.step(cltf, T=times)
+
+
     
-    # This manipulates the unity step input function to produce the error plot via
-    # subtraction from the step function and also creates the error value.
-    errY = []
-    err_val = 0
-    for s in range(timesteps):
-        errY.append(y1[s] - y2[s])
-        err_val = err_val + errY[s]
-    err_valsq = err_val*err_val
-    print("Fitness Error value = ", err_valsq, "    Error value: ", err_val)
-    
-    #Plotting graphs
+    """Plotting graphs
     fig, ax1 = pylab.subplots()
     ax1.plot(t1, errY,'g', linewidth=1.5)
     ax1.legend()
@@ -80,22 +42,13 @@ def initialise(cltf, ):
     ax1.axis([0, 3, 0, 1.2])
     ax2.axis([0, 3, 0, 1.2])
     pylab.grid()
-    pylab.show()
-
-
+    pylab.show()"""
+    return MSDnum, MSDden
 
 member = 0
 
-def fitness(member):
-    return 1
-    
-def crossover(a, b):
-    return a
-
-def mutate(member): 
-    return member
-
 def create_new_member():
+    """Creates the initial population of the genetic algorithm"""
     population = 40
     pop = []
     for s in range(population):
@@ -106,8 +59,56 @@ def create_new_member():
         kd_cur = round(random.uniform(kd_min, kd_max), 2)
         kp_cur = round(random.uniform(kp_min, kp_max), 2)
         ki_cur = round(random.uniform(ki_min, ki_max), 2)
+        #Into 2-D List. Access via pop[i][j]
         pop.insert(s, [kd_cur, kp_cur, ki_cur])
         print("Current Population Member: ", pop[s])
+    return population, pop
+
+
+def fitness(MSDnum, MSDden, population, pop):
+    """Calculates the fitness values of each member in pop[population]"""
+    for s in range(population):
+        #Create transfer functions for use by inputting current pop
+        GAnum = [pop[s][1], pop[s][2], pop[s][3]]
+        GAden = [0, 1, 0]
+        GHs_num = signal.convolve(GAnum, MSDnum)
+        GHs_den = signal.convolve(GAden, MSDden)
+        #Create CLTF
+        cltf_num = GHs_num
+        cltf_den = GHs_den + GHs_num
+        cltf = signal.TransferFunction(cltf_num, cltf_den)
+        
+        #Creating timestep table for step functions
+        times=[]
+        f=0.000
+        n = 0.001
+        timesteps = 3000
+        for m in range(timesteps):
+            times.append(f)
+            f = f+n
+
+        #create transfer function with no poles or zeros
+        unityF = signal.TransferFunction([1],[1])
+        # Step functions
+        t1, y1 = signal.step(unityF, T=times) #signal.step(errorsig)
+        t2, y2 = signal.step(cltf, T=times)
+        
+        # This is a subtraction from the step function creating an
+        # addition to the error value.
+        err_val[s] = [0]
+        fit_val = []
+        for o in range(timesteps):
+            err_val[s] = err_val[s] + y1[o] - y2[o]
+            fit_val[s] = err_val[s]*err_val[s]
+            print("Fitness Pop ", s, " error value = ", fit_val)
+    for m in range(population):
+        print("Fitness value for Pop ", s, " is :", fit_val[m])
+    return 1
+    
+def crossover(a, b):
+    return a
+
+def mutate(member): 
     return member
 
 def create_next_generation(population):
@@ -116,5 +117,6 @@ def create_next_generation(population):
 def main(number_of_iterations):
     return True
 
-#initialise()
+initialise()
 create_new_member()
+fitness(MSDnum, MSDden, population, pop)
