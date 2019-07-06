0d82c21106929926d8915253eae8628fc3019bb7
Ronan <ronancraig12@gmail.com>
2019-07-06 12:17:08 +0100

    Re-arrange and fitness function working
    initialise function removed since it wasn't needed. Parts moved to main() and fitness().
    Fitness now works.

--- GA V1_1.py
+++ GA V1_1.py
@@ -1,5 +1,6 @@
 """ Attempting to create the GA PID controller for the MSD system """
 #Created initial population
+#Fitness function working now
 
 import numpy as np
 from scipy import signal
@@ -9,48 +10,8 @@ from pylab import plot,xlabel,ylabel,title,legend,figure,subplots
 import matplotlib.pyplot as plt
 import random
 
-def initialise():
-    #first thing is to create the system itself - MSD, input
-    #MSD state definitions
-    m = 2 # kg   - mass
-    c = 10 # Ns/m - damping coefficient
-    k = 20 # N/m  - spring coefficient
-    
-    # PID initialising coefficients
-    kd = 0.0 # derivative gain
-    kp = 0.0 # proportional gain
-    ki = 0.0 # integral gain
-
-    # Transfer function definitions
-    MSDnum = [0, 0, 1]
-    MSDden = [m, c, k]
-
-
-
-
-    
-    """Plotting graphs
-    fig, ax1 = pylab.subplots()
-    ax1.plot(t1, errY,'g', linewidth=1.5)
-    ax1.legend()
-    ax1.set_xlabel('time (seconds)')
-    ax1.set_ylabel('Error',color='g')
-    pylab.title('Step Response and Error Signal')
-    ax2 = ax1.twinx()
-    ax2.set_ylabel('Amplitude',color='b')
-    ax2.plot(t2, y2,'b', label = r'$x (mm)$', linewidth=1)
-    ax1.axis([0, 3, 0, 1.2])
-    ax2.axis([0, 3, 0, 1.2])
-    pylab.grid()
-    pylab.show()"""
-    return MSDnum, MSDden
-
-member = 0
-
-def create_new_member():
+def create_new_member(population, pop):
     """Creates the initial population of the genetic algorithm"""
-    population = 40
-    pop = []
     for s in range(population):
         #Creating the random PID values
         kd_min, kd_max = 0, 100
@@ -61,15 +22,16 @@ def create_new_member():
         ki_cur = round(random.uniform(ki_min, ki_max), 2)
         #Into 2-D List. Access via pop[i][j]
         pop.insert(s, [kd_cur, kp_cur, ki_cur])
-        print("Current Population Member: ", pop[s])
-    return population, pop
+    return pop
 
 
 def fitness(MSDnum, MSDden, population, pop):
-    """Calculates the fitness values of each member in pop[population]"""
+    """Calculates the fitness values of each member in pop[population]
+    Also sets the simulation time in timesteps."""
+    fit_val = []
     for s in range(population):
         #Create transfer functions for use by inputting current pop
-        GAnum = [pop[s][1], pop[s][2], pop[s][3]]
+        GAnum = [pop[s][0], pop[s][1], pop[s][2]]   # kd, kp, ki 
         GAden = [0, 1, 0]
         GHs_num = signal.convolve(GAnum, MSDnum)
         GHs_den = signal.convolve(GAden, MSDden)
@@ -83,7 +45,7 @@ def fitness(MSDnum, MSDden, population, pop):
         f=0.000
         n = 0.001
         timesteps = 3000
-        for m in range(timesteps):
+        for p in range(timesteps):
             times.append(f)
             f = f+n
 
@@ -95,15 +57,16 @@ def fitness(MSDnum, MSDden, population, pop):
         
         # This is a subtraction from the step function creating an
         # addition to the error value.
-        err_val[s] = [0]
-        fit_val = []
+        err_vall = 0.0
+        err_val = 0.0
         for o in range(timesteps):
-            err_val[s] = err_val[s] + y1[o] - y2[o]
-            fit_val[s] = err_val[s]*err_val[s]
-            print("Fitness Pop ", s, " error value = ", fit_val)
+            # repeats every s repetition
+            err_vall = err_vall + y1[o] - y2[o]
+        err_val = err_vall * err_vall
+        fit_val.insert(s, err_val)
     for m in range(population):
-        print("Fitness value for Pop ", s, " is :", fit_val[m])
-    return 1
+        print("Fitness value for Pop Member ", m, " is :", fit_val[m])
+    return fit_val
     
 def crossover(a, b):
     return a
@@ -114,9 +77,47 @@ def mutate(member):
 def create_next_generation(population):
     return population
 
-def main(number_of_iterations):
+def main():
+    #first thing is to create the system itself - MSD, input
+    #MSD state definitions
+    m = 2 # kg   - mass
+    c = 10 # Ns/m - damping coefficient
+    k = 20 # N/m  - spring coefficient
+
+    # Transfer function definitions
+    MSDnum = [0, 0, 1]
+    MSDden = [m, c, k]
+    population = 40
+    pop = []
+
+    iteration = 0
+    while iteration < 100:
+        print(iteration)
+        if iteration == 0:
+            pop = create_new_member(population, pop)
+            fit_val = fitness(MSDnum, MSDden, population, pop)
+            iteration = iteration + 1
+
+        if iteration < 100:
+
+            iteration = iteration + 1
+        else:
+
+            """Plotting graphs
+            fig, ax1 = pylab.subplots()
+            ax1.plot(t1, errY,'g', linewidth=1.5)
+            ax1.legend()
+            ax1.set_xlabel('time (seconds)')
+            ax1.set_ylabel('Error',color='g')
+            pylab.title('Step Response and Error Signal')
+            ax2 = ax1.twinx()
+            ax2.set_ylabel('Amplitude',color='b')
+            ax2.plot(t2, y2,'b', label = r'$x (mm)$', linewidth=1)
+            ax1.axis([0, 3, 0, 1.2])
+            ax2.axis([0, 3, 0, 1.2])
+            pylab.grid()
+            pylab.show()"""
+
     return True
 
-initialise()
-create_new_member()
-fitness(MSDnum, MSDden, population, pop)
+main()
